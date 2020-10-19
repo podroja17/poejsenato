df75450a9e65094935385e96a03e354754bea223
podroja17 <podroja17@sps-prosek.cz>
2020-10-19 10:25:44 +0200

    Initial commit

---
+++ PoejSeNaTo/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PoejSeNaTo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            if (Device.RuntimePlatform == Device.iOS)
+            {
+                this.Padding = new Thickness(0, 20, 0, 0);
+            }
+            InitializeComponent();
+        }
+    }
+}
